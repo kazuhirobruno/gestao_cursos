498b7afc0341785400cf266f8764eaf1181c10ff
kazuhirobruno <brunokazuhiro@gmail.com>
2025-09-01 20:23:00 -0300

    feat: create initial structure

---
+++ src/test/java/br/com/kazuhiro/gestao_cursos/GestaoCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kazuhiro.gestao_cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
